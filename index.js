4100763ad36715a4147a1eb6cb1d47a4cdaac9d0
SuperLikeMike <119134973+SuperLikeMike@users.noreply.github.com>
2025-01-29 00:21:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Играет в /help", ""];
+const statusMessages = ["Играет в /help", "Играет в /help"];
 const statusTypes = [ 'idle', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
